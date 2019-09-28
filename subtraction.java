2870687931e562c1323091f4d050f833491f624b
Chvishnuvardhan707 <chinthalapudiv.vishnuvardhan@st.niituniversity.in>
2019-09-28 11:33:01 +0530

    Subtraction added

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Subtraction implements Operation
+{
+	public String method(double a,double b){
+		System.out.println("Subtraction selected");
+		String x = Double.toString(a-b);
+		return x;
+	} 
+}
